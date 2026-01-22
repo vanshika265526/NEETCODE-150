6063a58a83075f50d86f403add17e3d8c78ebdce
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-01-22 22:17:17 +0530

    Create Group Anagrams.cpp

---
+++ Arrays&Hashing/Group Anagrams.cpp
@@ -0,0 +1,30 @@
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+*/
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for (auto &it : mp) {
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
