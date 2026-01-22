299e373593f138b528de66a5d5284b10968803c5
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-01-22 21:43:53 +0530

    Create 2Sum - input array sorted.cpp

---
+++ Two Pointers/2Sum - input array sorted.cpp
@@ -0,0 +1,33 @@
+/*
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+  */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0, right = numbers.size() - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                // Add +1 because question wants 1-indexed answer
+                return {left + 1, right + 1};
+            } 
+            else if (sum < target) {
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+
+        //if no solution
+        return {};
+    }
+};
