753c1485c7aa0e6e33374c54f88bce41434bf6a6
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-01-24 18:31:22 +0530

    Create Product of Array Except Self.cpp

---
+++ Arrays&Hashing/Product of Array Except Self.cpp
@@ -0,0 +1,24 @@
+/*
+  Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+*/
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> result(nums.size(),1);
+        int prefix=1;
+        for(int i=0;i<nums.size();i++){
+            result[i]=prefix;
+            prefix*=nums[i];
+        }
+        int suffix=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            result[i]*=suffix;
+            suffix*=nums[i];
+        }
+        return result;
+    }
+};
