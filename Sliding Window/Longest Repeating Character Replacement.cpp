f75b13caadb1b22d060015a16e7872c48671a2c5
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-01-28 15:43:55 +0530

    Create Longest Repeating Character Replacement.cpp

---
+++ Sliding Window/Longest Repeating Character Replacement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int left=0;
+        int res=0;
+        int maxf=0;
+        unordered_map<char,int> count;
+        for(int right=0;right<s.size();right++){
+            count[s[right]]++;
+            maxf=max(maxf,count[s[right]]);
+            while((right-left+1)-maxf>k){
+                count[s[left]]--;
+                left++;
+            }
+            res=max(res,right-left+1);
+            
+        }
+        return res;
+
+
+    }
+};
