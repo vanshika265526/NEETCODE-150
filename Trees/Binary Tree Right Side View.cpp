76bde14a88980be3bd8476250a51901669969c08
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-04-16 09:46:38 +0530

    Create Binary Tree Right Side View.cpp

---
+++ Trees/Binary Tree Right Side View.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        if(!root) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            int rightmost;
+            for(int i=0;i<size;i++){
+                TreeNode* front=q.front();
+                q.pop();
+                rightmost=front->val;
+                if(front->left) q.push(front->left);
+                if(front->right) q.push(front->right);
+
+            }
+            result.push_back(rightmost);
+        }
+        return result;
+        
+    }
+};
