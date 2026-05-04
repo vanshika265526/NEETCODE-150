71ca142819bae8c80f6fa4bcc51919e234b80895
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-05-04 17:41:48 +0530

    Update Pow(x, n).cpp

--- Math and Geometry/Pow(x, n).cpp
+++ Math and Geometry/Pow(x, n).cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     double myPow(double x, int n) {
         long long N = n;   // avoid overflow
-    double ans = 1;
-
-    if (N < 0) {
-        x = 1 / x;
-        N = -N;
-    }
-    while (N > 0) {
-        if (N % 2 == 1) {   // if odd
-            ans = ans * x;
+        double ans = 1;
+    
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
         }
-
-        x = x * x;   // square
-        N = N / 2;   // halve
-    }
-
-    return ans;
-        
-    }
+        while (N > 0) {
+            if (N % 2 == 1) {   // if odd
+                ans = ans * x;
+            }
+    
+            x = x * x;   // square
+            N = N / 2;   // halve
+        }
+    
+        return ans;
+            
+        }
 };
