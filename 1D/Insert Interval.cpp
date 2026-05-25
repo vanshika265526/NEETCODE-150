4be201d1ac1f5dc12c8c7cfae6da7df52501f4f0
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-05-25 09:54:24 +0530

    Update Insert Interval.cpp

--- 1D/Insert Interval.cpp
+++ 1D/Insert Interval.cpp
@@ -1,28 +1,31 @@
-function insert(intervals, newInterval) {
-    let result = [];
-    let i = 0;
-    let n = intervals.length;
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n=intervals.size();
+        int i=0;
+        vector<vector<int>> result;
 
-    // 1. Add intervals before newInterval
-    while (i < n && intervals[i][1] < newInterval[0]) {
-        result.push(intervals[i]);
-        i++;
+        //add before wale intervals here
+        while(i<n && intervals[i][1]<newInterval[0]){
+            result.push_back(intervals[i]);
+            i++;
+        }
+
+        //merge
+        while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1],intervals[i][1]);
+            i++;
+        }
+        result.push_back(newInterval);
+
+        //after elements
+        while(i<n){
+            result.push_back(intervals[i]);
+            i++;
+        }
+        return result;
+
+        
     }
-
-    // 2. Merge overlapping intervals
-    while (i < n && intervals[i][0] <= newInterval[1]) {
-        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-        i++;
-    }
-
-    result.push(newInterval);
-
-    // 3. Add remaining intervals
-    while (i < n) {
-        result.push(intervals[i]);
-        i++;
-    }
-
-    return result;
-}
+};
