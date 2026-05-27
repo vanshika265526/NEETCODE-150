684220cd63f52446cfdb0f2518fa1ed18f22921c
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-05-27 18:30:53 +0530

    Create Meeting Rooms II.cpp

---
+++ Intervals/Meeting Rooms II.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(),
+            [](Interval a, Interval b) {
+                return a.start < b.start;
+            });
+            priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(int i = 0; i < intervals.size(); i++) {
+
+            // if some room becomes free
+            if(!pq.empty() && intervals[i].start >= pq.top()) {
+                pq.pop();
+            }
+
+            // occupy room
+            pq.push(intervals[i].end);
+        }
+
+        return pq.size();
+    }
+};
